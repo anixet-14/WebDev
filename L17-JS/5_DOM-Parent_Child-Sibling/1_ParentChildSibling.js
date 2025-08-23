68e49afc774d75782047ac662586647d84416505
Aniket Gupta <aniket.ag1408@gmail.com>
2025-08-23 13:52:20 +0530

    DOM

---
+++ L17-JS/5_DOM-Parent_Child-Sibling/1_ParentChildSibling.js
@@ -0,0 +1,11 @@
+let movieList = document.querySelector('.movieList')
+//To access children
+console.log(movieList.children)
+
+let movie = document.querySelector('.movie')
+//To access parent
+console.log(movie.parentElement)
+//Previous sibling
+console.log(movie.previousElementSibling)
+//Next sibling
+console.log(movie.nextElementSibling)
